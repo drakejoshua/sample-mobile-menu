dba93ce5610e4e568b0feb050128fcaf23e4e44a
Joshua Mabawonku <drakejoshua086@gmail.com>
2023-07-27 22:18:50 +0100

    ADDED INITIAL FILES
    - added inital files for the repo( index.html, style.css .. )

---
+++ script.js
@@ -0,0 +1,13 @@
+function displayMenu()
+{
+    var menu = document.getElementById("dropdown");
+
+    if ( menu.style.height == "0px" || menu.style.height == "" )
+    {
+        menu.style.height = "200px";
+    }
+    else
+    {                            
+        menu.style.height = "0px";
+    }
+}
